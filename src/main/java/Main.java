d19ff75feda2086813107305589d85954526c2e6
Alejandro Chavez <alejandrochavez2323@gmail.com>
2024-05-30 11:13:32 -0500

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7001);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
